f09ab0dc31074ab374d9d9f8e81b06a7035450ca
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2022-05-07 19:12:20 +0500

    запустил локалку

--- startup.sh
+++ startup.sh
@@ -1,6 +1,11 @@
 FOLDER_ID=b1g3hblb6k8v0sll50g1
 DB_NAME=todo
 
+docker-compose -f docker-compose-postgres.yml up -d --build
+docker-compose up -d --build
+
+exit 0
+
 yc ydb database create $DB_NAME \
     --serverless \
     --folder-id $FOLDER_ID \
