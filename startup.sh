dd3bc7fddc9f8b15307ff8f7a466512a0ea1b388
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2022-05-07 14:55:51 +0500

    typo

--- startup.sh
+++ startup.sh
@@ -2,5 +2,5 @@ FOLDER_ID=b1g3hblb6k8v0sll50g1
 DB_NAME=todo
 
 yc ydb database create $DB_NAME --serverless --folder-id $FOLDER_ID
-DB_ID=$(yc ydb database get --name $DB_NAME --folder-id $FOLDER_ID | grep document_api_endpoint: | awk '{print $2}')
-echo $DB_ID > variables/DB_ID
+DOCUMENT_API_ENDPOINT=$(yc ydb database get --name $DB_NAME --folder-id $FOLDER_ID | grep document_api_endpoint: | awk '{print $2}')
+echo $DOCUMENT_API_ENDPOINT > variables/DOCUMENT_API_ENDPOINT
