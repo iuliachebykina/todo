78563ac4317863689d6377216ea8b1bc2b984ced
MehLater <Artem.Zavialov@urfu.me>
2022-05-09 15:49:26 +0500

    Random selection

--- frontend/src/app/List/List.tsx
+++ frontend/src/app/List/List.tsx
@@ -1,18 +1,17 @@
 import React, {useEffect, useReducer, useState} from "react"
+import DataProvider from "../DataProvider"
 import { ListElement } from "../interfaces/ListInterface"
 import {getAllTodos} from "../DataProvider";
 import {deleteTodo} from "../DataProvider";
-import {editTodo} from "../DataProvider";
 
 const TodoElement = (element: ListElement, index: number, forceUpdate: React.DispatchWithoutAction, selectedIndex: number) => {
-    const del = async () => {
-        await deleteTodo(element.id)
+    const del = () => {
+        if (DataProvider.Table.SelectedIndex === index)
+            DataProvider.Table.SelectedIndex = undefined;
+        deleteTodo(index)
         forceUpdate();
     }
-    const edit = async () => {
-        await editTodo(element.id, prompt("Input new value") ?? element.description);
-        forceUpdate();
-    }
+    console.log(selectedIndex);
 
     return (
         <div key={index}
@@ -22,25 +21,22 @@ const TodoElement = (element: ListElement, index: number, forceUpdate: React.Dis
             <div className="w-0.5 h-5/6 bg-gray-400 mx-1 sm:mx-3"/>
             <div className="w-fit">{element.description}</div>
             <div className="ml-auto select-none cursor-pointer"
-                 onClick={edit}>✏</div>
-            <div className="select-none cursor-pointer"
             onClick={del}>❌</div>
         </div>
     )
 }
 
-const TodoList = ({setCurrentListLength, selectedIndex, update, forceUpdate}: {setCurrentListLength: (length: number) => any,
-    selectedIndex: number,
-    update: any,
-    forceUpdate: () => any}) => {
+const TodoList = ({setCurrentListLength, selectedIndex}: {setCurrentListLength: (length: number) => any,
+    selectedIndex: number}) => {
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
     const [elements, setElements] = useState<JSX.Element[]>([]);
     useEffect(() => {
         getAllTodos().then(res => {
-            let test = res.data.map((e,i) => TodoElement({id: e.id, description: e.task}, i, forceUpdate, selectedIndex));
+            let test = res.data.map(e => TodoElement({id: e.id, description: e.task}, e.id, forceUpdate, selectedIndex));
             setElements(test);
             setCurrentListLength(elements.length);
         });
-    }, [update]);
+    });
 
     return (
         <div className="px-2 sm:px-4 mb-20 sm:mb-24" id="todo-list">
